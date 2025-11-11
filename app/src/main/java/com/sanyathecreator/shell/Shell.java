ca4336750cf83c5e10c3ad473d127483a265ddb8
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2025-11-11 16:36:47 +0100

    feat: invalid command handling

--- app/src/main/java/com/sanyathecreator/shell/Shell.java
+++ app/src/main/java/com/sanyathecreator/shell/Shell.java
@@ -1,9 +1,19 @@
 package com.sanyathecreator.shell;
 
+import java.util.Scanner;
+
 public class Shell {
 
     public void start() {
-        System.out.println("$ ");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("$ ");
+            String input = scanner.nextLine();
+            handleInvalidCommand(input);
+        }
     }
 
+    private void handleInvalidCommand(String input) {
+        System.out.println(input + ": command not found");
+    }
 }

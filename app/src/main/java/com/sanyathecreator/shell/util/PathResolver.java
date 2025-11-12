419b88ac945a9b7d9eb76644bb54313bc730b9b1
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2025-11-12 11:22:14 +0100

    feat: add PathResolver utility for finding executables in PATH

---
+++ app/src/main/java/com/sanyathecreator/shell/util/PathResolver.java
@@ -0,0 +1,29 @@
+package com.sanyathecreator.shell.util;
+
+import java.io.File;
+
+import com.sanyathecreator.shell.ShellContext;
+
+public class PathResolver {
+
+    public static String findExecutableInPath(String command) {
+        String pathEnv = ShellContext.PATH;
+
+        String[] splitDirs = pathEnv.split(File.pathSeparator);
+        for (String dir : splitDirs) {
+            File directory = new File(dir);
+            if (!directory.exists() || !directory.isDirectory()) {
+                continue;
+            }
+
+            // Find executable file in directory
+            File commandFile = new File(directory, command);
+            if (commandFile.exists() && commandFile.canExecute()) {
+                return commandFile.getAbsolutePath();
+            }
+        }
+
+        return null;
+    }
+
+}

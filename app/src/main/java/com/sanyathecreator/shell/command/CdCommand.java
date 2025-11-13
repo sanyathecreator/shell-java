0909527d99418d7b60a888fe9ba0fbabc9774679
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2025-11-13 09:51:48 +0100

    feat: implement relative path handling for cd command

--- app/src/main/java/com/sanyathecreator/shell/command/CdCommand.java
+++ app/src/main/java/com/sanyathecreator/shell/command/CdCommand.java
@@ -9,14 +9,22 @@ public class CdCommand implements Command {
     @Override
     public void execute(String[] args) {
         String path = args[0];
-        File directory = new File(path);
+        File directory;
+
+        if (path.startsWith("/")) {
+            // Absolute path
+            directory = new File(path);
+        } else {
+            // Relative path
+            directory = new File(ShellContext.getCurrentDirectory(), path);
+        }
 
         if (!directory.exists() || !directory.isDirectory()) {
             System.out.println("cd: " + path + ": No such file or directory");
             return;
         }
 
-        ShellContext.setCurrentDirectory(path);
+        ShellContext.setCurrentDirectory(directory.getAbsolutePath());
     }
 
 }
